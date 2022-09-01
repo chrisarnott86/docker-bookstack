64d2bcf26ac47ab870abc36513435255476076c8
chrisarnott86 <chrisarnott86@hotmail.com>
2022-09-01 14:25:28 +0100

    Update database.php

--- database.php
+++ database.php
@@ -76,10 +76,10 @@ return [
             'prefix_indexes' => true,
             'strict'         => false,
             'engine'         => null,
-            'options' => array(
-                        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ),
+            'options'        => [
+                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]
             //'options'        => extension_loaded('pdo_mysql') ? array_filter([
             //    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             //    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
